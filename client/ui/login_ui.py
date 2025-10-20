2d3f178ee078b94eb630c7289c67e719c67e9551
BrianLe54 <legiabao0504@gmail.com>
2025-10-21 00:36:03 +0700

    Chỉnh lại file login_ui

--- client/ui/login_ui.py
+++ client/ui/login_ui.py
@@ -1,35 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+
 class LoginUI:
-    def __init__(self, root):
-        self.root = root
-        # Tạo một frame chính để chứa các giao diện khác nhau
-        self.main_frame = tk.Frame(self.root)
-        self.main_frame.pack(fill="both", expand=True, padx=20, pady=20)
-
-        # Cấu hình style cho các widget của ttk
-        self.style = ttk.Style()
-        self.style.configure('Accent.TButton', font=('Arial', 14))
-
-    def clear_main_frame(self):
-        """Xóa tất cả widget con trong main_frame."""
-        for widget in self.main_frame.winfo_children():
-            widget.destroy()
-
-    def create_mode_selection_ui(self):
-        self.clear_main_frame()
-        self.mode_selection_frame = ttk.Frame(self.main_frame)
-        self.mode_selection_frame.pack(expand=True)
-
-        ttk.Label(self.mode_selection_frame, text="CHỌN CHẾ ĐỘ CHƠI", font=("Arial", 24, "bold")).pack(pady=20)
-        ttk.Button(self.mode_selection_frame, text="Chơi Online", style='Accent.TButton', command=self.show_login_ui).pack(pady=10, ipady=5, ipadx=10)
-        ttk.Button(self.mode_selection_frame, text="Chơi Offline (Với Máy)", command=self.start_offline_game).pack(pady=10, ipady=5, ipadx=10)
-
-         # Cấu hình style cho nút nhấn
-        s = ttk.Style()
-        s.configure('Accent.TButton', font=('Arial', 14), background='#3498DB', foreground='white')
-        s.map('Accent.TButton', background=[('active', '#2980B9')])
-    
     def show_login_ui(self):
         self.clear_main_frame()
         self.login_frame = ttk.Frame(self.main_frame)
@@ -47,6 +19,4 @@ class LoginUI:
         self.ip_entry.pack(pady=5, padx=20)
 
         ttk.Button(self.login_frame, text="Tìm trận", style='Accent.TButton', command=self.connect_to_server).pack(pady=20, ipady=5, ipadx=10)
-    
-    def run(self):
-        self.mainloop()
+
